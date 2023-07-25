538c1ed7bd4d1dfa9e27b8fb52d02fd2e47a9434
avelinojrr <axelavelino1@gmail.com>
2023-07-25 12:28:46 -0400

    agregamos la funcion que incrypta las contraseñas con la dependencia bcryptjs

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
     const {
@@ -8,14 +10,25 @@ export const register = async (req, res) => {
     } = req.body
 
     try {
+
+        const passwordHash = await bcrypt.hash(password, 10);
+
+
         const newUser = new User({
             username,
             email,
-            password
+            password: passwordHash,
         });
 
         const userSaved = await newUser.save();
-        res.json(userSaved)
+
+        res.json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createdAt: userSaved.createdAt,
+            udpatedAt: userSaved.updatedAt,
+        })
     } catch (error) {
         console.log(error);
     }
